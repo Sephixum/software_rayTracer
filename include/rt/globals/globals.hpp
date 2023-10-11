aceee21f8c2cacfb017fa7c7cbd1827b4c31b097
Sephixum <emotive.5353@gmail.com>
2023-10-11 17:43:09 +0330

    Restructured

--- include/rt/globals/globals.hpp
+++ include/rt/globals/globals.hpp
@@ -2,8 +2,8 @@
 
 namespace rt {
 namespace globals {
-constexpr int kImageWidth = 200;
-constexpr int kImageHeight = 100;
+constexpr int kImageWidth = 800;
+constexpr int kImageHeight = 400;
 constexpr float kGradientStrength{0.5f};
 } // namespace globals
 } // namespace rt
